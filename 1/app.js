c08642804c2131343a99a7f222fc36c134ed21de
Murat Can Berber <30313472+muratcanberber@users.noreply.github.com>
2023-01-06 01:33:52 +0300

    v-cloak directive

--- 1/app.js
+++ 1/app.js
@@ -6,10 +6,15 @@ const vm = Vue.createApp({
       lastName: "Dogget",
     };
   },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+  },
 }).mount("#app");
 //bind vue to the #app div in the html
 
-setTimeout(() => {
-  vm.firstName = "Bob";
-  //we dont need to specify vm.data... vue does this magicly
-}, 5000);
+// setTimeout(() => {
+//   vm.firstName = "Bob";
+//   //we dont need to specify vm.data... vue does this magicly
+// }, 5000);
