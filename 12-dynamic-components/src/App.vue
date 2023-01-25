22dd74070d1c2e3a9bd5f7a36162a025b6069e2f
Murat Can Berber <30313472+muratcanberber@users.noreply.github.com>
2023-01-25 17:01:52 +0300

    Dynamic Components
    <Components> can be use for displaying dynamic components. Avoding not displaying components to be destroy we can use KeepAlive.

---
+++ 12-dynamic-components/src/App.vue
@@ -0,0 +1,27 @@
+
+import About from './components/About.vue';
+
+<template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <KeepAlive>
+    <Component :is="componentName" />
+  </KeepAlive>
+</template>
+
+<script>
+import About from './components/About.vue';
+import Home from './components/Home.vue';
+
+export default {
+  name: "App",
+  components: { Home, About },
+  data() {
+    return {
+      componentName: "Home"
+    }
+  }
+}
+</script>
---
+++ 12-dynamic-components/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>Home</h1>
+</template>
+
+<script>
+export default {
+	unmounted() {
+		console.log("Home unmounted")
+		//In KeepAlive this wont work
+		//activated and deactived hooks will work
+	},
+	activated() {
+		console.log("Home activated")
+	},
+	deactivated() {
+		console.log("Home deactivated")
+	}
+}
+</script>
