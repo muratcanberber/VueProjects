73543e4763a5a9fee4149d9c853071eafa10fc05
Murat Can Berber <30313472+muratcanberber@users.noreply.github.com>
2023-01-25 16:39:44 +0300

    Slots 2

--- 11-slots/src/App.vue
+++ 11-slots/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <Form>
-    <div class="help">
-      <p>This is some help text.</p>
-    </div>
-    <div class="fields">
+    <template v-slot:help>
+      <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-    </div>
-    <div class="buttons">
+    </template>
+    <template v-slot:buttons>
       <button type="submit">Submit</button>
-    </div>
+    </template>
+    <p>lorem10</p>
   </Form>
-  <Form />
+  <Form>
+    <template v-slot:help>
+      <p>Contact help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </Form>
 </template>
 
 <script>
@@ -21,5 +33,10 @@ import Form from "./components/Form.vue";
 export default {
   name: "App",
   components: { Form },
+  data() {
+    return {
+      help: "This is some help text",
+    };
+  },
 };
 </script>
--- 11-slots/src/components/Form.vue
+++ 11-slots/src/components/Form.vue
@@ -1,3 +1,22 @@
 <template>
-  <form><slot>No Form to render</slot></form>
+  <form>
+    <div class="help">
+      <slot name="help">
+        <p>This is some help text.</p>
+      </slot>
+    </div>
+    <div class="fields">
+      <slot name="fields">
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+      </slot>
+    </div>
+    <div class="buttons">
+      <slot name="button">
+        <button type="submit">Submit</button>
+      </slot>
+    </div>
+    <slot></slot>
+  </form>
 </template>
